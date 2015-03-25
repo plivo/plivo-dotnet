6fb6f6fcc26b3bbf01a000137dd9e99773026f9c
Shaun Bowe <shaunbowe@gmail.com>
2015-03-25 14:43:31 -0500

    Add call_duration and billed_duration

--- Plivo/Models.cs
+++ Plivo/Models.cs
@@ -301,18 +301,20 @@ namespace Plivo.API
 
     public class CDR
     {
-        public int bill_duration { get; set; }
-        public string total_amount { get; set; }
-        public string parent_call_uuid { get; set; }
-        public string call_direction { get; set; }
-        public string to_number { get; set; }
-        public string total_rate { get; set; }
-        public string from_number { get; set; }
-        public string end_time { get; set; }
-        public string call_uuid { get; set; }
-        public string resource_uri { get; set; }
-        public string error { get; set; }
         public string api_id { get; set; }
+        public int bill_duration { get; set; }
+        public int billed_duration { get; set; }
+        public string call_direction { get; set; }
+        public int call_duration { get; set; }
+        public string call_uuid { get; set; }
+        public string end_time { get; set; }
+        public string from_number { get; set; }
+        public string parent_call_uuid { get; set; }
+        public string resource_uri { get; set; }
+        public string to_number { get; set; }
+        public string total_amount { get; set; }
+        public string total_rate { get; set; }
+        public string error { get; set; }
     }
 
     public class CDRList
