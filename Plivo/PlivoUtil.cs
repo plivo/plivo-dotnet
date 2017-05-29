4916756dbab814349924cf0c3d0357411f26dcac
Kumaresan Rajeswaran <kumaresan.rajeswaran@gmail.com>
2017-05-29 16:50:46 +0530

    Adding additional signature verify helper for GET requests

--- Plivo/PlivoUtil.cs
+++ Plivo/PlivoUtil.cs
@@ -10,6 +10,36 @@ namespace Plivo.Util
 {
     public class XPlivoSignature
     {
+        
+        /// <summary>
+        ///   Verifes signature of a request, specifically for GET requests.
+        /// </summary>
+        /// <param name="uri">URL received by server</param>
+        /// <param name="originalUri">Original URL sent to Plivo or configured in application</param>
+        /// <param name="xPlivoSignature">Expected Signature obtained from 'X-Plivo-Signature' header</param>
+        /// <param name="authToken">Plivo Auth token</param>
+        /// <returns>true if signature matches, false otherwise</returns>
+        public static bool Verify(string uri, string originalUri, string xPlivoSignature, string authToken)
+        {
+            var qs = new Uri(uri).Query;
+            var qsDict = System.Web.HttpUtility.ParseQueryString(qs);
+            
+            var dict = qsDict.AllKeys.ToDictionary(item => item, item => qsDict[item]);
+            
+            // use original url 
+            foreach (KeyValuePair<string, string> kvp in dict.OrderBy(key => key.Key, StringComparer.Ordinal))
+                originalUri += kvp.Key + kvp.Value;
+
+            var myhmacsha1 = new HMACSHA1(Encoding.UTF8.GetBytes(authToken));
+            var byteArray = Encoding.UTF8.GetBytes(originalUri);
+            var stream = new MemoryStream(byteArray);
+            var hashValue = myhmacsha1.ComputeHash(stream);
+            var generatedSignature = Convert.ToBase64String(hashValue);
+
+            return xPlivoSignature.Equals(generatedSignature);
+        }
+        
+        
         public static bool Verify(string uri, dict plivoHttpParams, string xPlivoSignature, string authToken)
         {
             var isMatch = false;
