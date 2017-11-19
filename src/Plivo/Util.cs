55d7aedb09273153b0225b56479354d9b80d2f8c
Chris Christou <biochristou@users.noreply.github.com>
2017-11-19 00:33:55 -0700

    Moved XPlivoSignature to the Plivo.Utilities namespace

--- src/Plivo/Util.cs
+++ src/Plivo/Util.cs
@@ -16,25 +16,6 @@ namespace Plivo
     public class Util
     {
         /// <summary>
-        /// XPlivoSignature
-        /// </summary>
-        public class XPlivoSignature
-        {
-            /// <summary>
-            /// Verify the signature
-            /// </summary>
-            /// <param name="uri"></param>
-            /// <param name="plivoHttpParams"></param>
-            /// <param name="xPlivoSignature"></param>
-            /// <param name="authToken"></param>
-            /// <returns></returns>
-            public static bool Verify(string uri, Dictionary<string, object> plivoHttpParams, string xPlivoSignature, string authToken)
-            {
-                return false;
-            }
-        }
-
-        /// <summary>
         /// HtmlEntity.
         /// </summary>
         public class HtmlEntity
---
+++ src/Plivo/Utilities/XPlivoSignature.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace Plivo.Utilities
+{
+	/// <summary>
+	/// XPlivoSignature
+	/// </summary>
+	public class XPlivoSignature
+	{
+		/// <summary>
+		/// Verify the signature
+		/// </summary>
+		/// <param name="uri"></param>
+		/// <param name="plivoHttpParams"></param>
+		/// <param name="xPlivoSignature"></param>
+		/// <param name="authToken"></param>
+		/// <returns></returns>
+		public static bool Verify(string uri, Dictionary<string, object> plivoHttpParams, string xPlivoSignature, string authToken)
+		{
+			return false;
+		}
+	}
+}
