805c1bb4a9265bbe065d3a4e71727ac2492a32c9
Manish Chand Kaushik <manish@plivo.com>
2018-09-06 18:16:33 +0530

    to string update

--- src/Plivo/Resource/Call/LiveCall.cs
+++ src/Plivo/Resource/Call/LiveCall.cs
@@ -13,5 +13,18 @@ namespace Plivo.Resource.Call
         public string CallUuid { get; set; }
         public string RequestUuid { get; set; }
         public string SessionStart { get; set; }
+
+        public override string ToString()
+        {
+            return base.ToString() +
+                       "Direction: " + Direction + "\n" +
+                       "From: " + From + "\n" +
+                       "CallStatus: " + CallStatus + "\n" +
+                       "To: " + To + "\n" +
+                       "CallerName: " + CallerName + "\n" +
+                       "CallUuid: " + CallUuid + "\n" +
+                       "RequestUuid: " + RequestUuid + "\n" +
+                       "SessionStart: " + SessionStart + "\n";
+        }
     }
 }
--- src/Plivo/Resource/Call/LiveCallListResponse.cs
+++ src/Plivo/Resource/Call/LiveCallListResponse.cs
@@ -6,5 +6,12 @@ namespace Plivo.Resource.Call
     {
         public string ApiId { get; set; }
         public List<string> Calls { get; set; }
+
+        public override string ToString()
+        {
+            return base.ToString() +
+                       "ApiId: \n" + ApiId + "\n" +
+                       "Calls: \n" + string.Join(",\n", Calls ) + "\n";
+        }
     }
 }
