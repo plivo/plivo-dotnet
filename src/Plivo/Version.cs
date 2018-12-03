78fc05db6222e47f66ffcddae2dac268b20e410e
suresach <sachin.k@plivo.com>
2018-12-04 00:48:53 +0530

    upgrade version to 4.2.0

--- src/Plivo/Version.cs
+++ src/Plivo/Version.cs
@@ -10,7 +10,7 @@ namespace Plivo
         /// <summary>
         /// DotNet SDK version
         /// </summary>
-        public const string SdkVersion = "4.1.7";
+        public const string SdkVersion = "4.2.0";
 
         /// <summary>
         /// Plivo API version
