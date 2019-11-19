7b5b121b1a60738dd48f210a6b12244b03688bbe
Narayana Shanubhogh <40889842+narayana-plivo@users.noreply.github.com>
2019-11-19 11:25:36 +0530

    Update Numbers.cs

--- src/Plivo/Resource/Powerpack/Numbers.cs
+++ src/Plivo/Resource/Powerpack/Numbers.cs
@@ -1,26 +1,23 @@
 using System;
 using System.Threading.Tasks;
 using Plivo.Resource.PhoneNumber;
-namespace Plivo.Resource.Powerpack
-{
+namespace Plivo.Resource.Powerpack {
     /// <summary>
     /// Numberpool.
     /// </summary>
-    public class Numbers : Resource
-    {
+    public class Numbers : Resource {
         private string numberpooluuid = "";
-        public Numbers(string number_pool_id)
-        {
+        public Numbers (string number_pool_id) {
             numberpooluuid = number_pool_id;
         }
-        public Numbers(){
+        public Numbers () {
 
         }
         public Lazy<PhoneNumberInterface> _phonenumber;
 
         public Lazy<PowerpackInterface> _powerpackInterface;
 
-        private PowerpackInterface PowerpackI =>_powerpackInterface.Value;
+        private PowerpackInterface PowerpackI => _powerpackInterface.Value;
         private PhoneNumberInterface PhoneNumberI => _phonenumber.Value;
         public new string Id => NumberPoolUuid;
 
@@ -60,108 +57,91 @@ namespace Plivo.Resource.Powerpack
         /// <value>The Type</value>
         public string Type { get; set; }
 
-        public Numbers Get(string memberId)
-        {
-            var member = new Numbers();
+        public Numbers Get (string memberId) {
+            var member = new Numbers ();
             return member;
         }
-        public ListResponse<Numbers> List(string starts_with = null, string country_iso2 = null,
-        string type = null, uint? limit = null, uint? offset = null)
-        {
-            return PowerpackI.List_Numbers(numberpooluuid, starts_with, country_iso2, type, limit, offset);
+        public ListResponse<Numbers> List (string starts_with = null, string country_iso2 = null,
+            string type = null, uint? limit = null, uint? offset = null) {
+            return PowerpackI.List_Numbers (numberpooluuid, starts_with, country_iso2, type, limit, offset);
         }
-        public async Task<ListResponse<Numbers>> ListAsync(
-          string starts_with = null, string country_iso2 = null,
-          string type = null, uint? limit = null, uint? offset = null)
-        {
+        public async Task<ListResponse<Numbers>> ListAsync (
+            string starts_with = null, string country_iso2 = null,
+            string type = null, uint? limit = null, uint? offset = null) {
             return await PowerpackI
-                .List_NumbersAsync(numberpooluuid, starts_with, country_iso2, type, limit, offset);
+                .List_NumbersAsync (numberpooluuid, starts_with, country_iso2, type, limit, offset);
 
         }
 
-        public uint Count(string starts_with = null, string country_iso2 = null,
-        string type = null, uint? limit = null, uint? offset = null)
-        {
-            
+        public uint Count (string starts_with = null, string country_iso2 = null,
+            string type = null, uint? limit = null, uint? offset = null) {
+
             return PowerpackI
-                               .Count_Number(numberpooluuid, starts_with, country_iso2, type, limit, offset);
+                .Count_Number (numberpooluuid, starts_with, country_iso2, type, limit, offset);
         }
-        public async Task<uint> CountAsync(
-          string starts_with = null, string country_iso2 = null,
-          string type = null, uint? limit = null, uint? offset = null)
-        {
+        public async Task<uint> CountAsync (
+            string starts_with = null, string country_iso2 = null,
+            string type = null, uint? limit = null, uint? offset = null) {
             return await PowerpackI
-                .Count_NumbersAsync(numberpooluuid, starts_with, country_iso2, type, limit, offset);
+                .Count_NumbersAsync (numberpooluuid, starts_with, country_iso2, type, limit, offset);
 
         }
-        public Numbers Add(string number)
-        {
+        public Numbers Add (string number) {
             return PowerpackI
-                               .Add_Number(numberpooluuid, number);
+                .Add_Number (numberpooluuid, number);
         }
-        public async Task<Numbers> AddAsync(string number)
-        {
+        public async Task<Numbers> AddAsync (string number) {
             return await PowerpackI
-                .Add_NumberAsync(numberpooluuid, number);
+                .Add_NumberAsync (numberpooluuid, number);
         }
 
-        public DeleteResponse<Numbers> Remove(string number, bool? unrent = null)
-        {
+        public DeleteResponse<Numbers> Remove (string number, bool? unrent = null) {
             return PowerpackI
-                               .Remove_Number(numberpooluuid, number, unrent);
+                .Remove_Number (numberpooluuid, number, unrent);
         }
-        public async Task<DeleteResponse<Numbers>> RemoveAsync(
-            string number, bool? unrent = null)
-        {
+        public async Task<DeleteResponse<Numbers>> RemoveAsync (
+            string number, bool? unrent = null) {
             return await PowerpackI
-                .Remove_NumberAsync(numberpooluuid, number, unrent);
+                .Remove_NumberAsync (numberpooluuid, number, unrent);
         }
 
-        public Numbers Find(string number)
-        {
+        public Numbers Find (string number) {
             return PowerpackI
-                               .Find_Number(numberpooluuid, number);
+                .Find_Number (numberpooluuid, number);
         }
-        public async Task<Numbers> FindAsync(
-            string number)
-        {
+        public async Task<Numbers> FindAsync (
+            string number) {
             return await PowerpackI
-                .Find_NumberAsync(numberpooluuid, number);
+                .Find_NumberAsync (numberpooluuid, number);
         }
-        
 
-        public Numbers Buy_Add_Number(string number = null, string type = null, string country_iso2 = null, string region = null, string pattern = null)
-        {
-            if (number == null)
-            {
+        public Numbers Buy_Add_Number (string number = null, string type = null, string country_iso2 = null, string region = null, string pattern = null) {
+            if (number == null) {
                 var countryIso = country_iso2;
-                var numbers = PhoneNumberI.List(countryIso, type, pattern, region);
+                var numbers = PhoneNumberI.List (countryIso, type, pattern, region);
                 number = numbers.Objects[0].Number;
             }
             return PowerpackI
-                               .Add_Number(numberpooluuid, number, true);
+                .Add_Number (numberpooluuid, number, true);
         }
-        public async Task<Numbers> Buy_Add_NumberAsync(string number = null, string type = null, string country_iso2 = null, string region = null, string pattern = null)
-        {
-            if (number == null)
-            {
+        public async Task<Numbers> Buy_Add_NumberAsync (string number = null, string type = null, string country_iso2 = null, string region = null, string pattern = null) {
+            if (number == null) {
                 var countryIso = country_iso2;
-                var numbers = PhoneNumberI.List(countryIso, type, pattern, region);
+                var numbers = PhoneNumberI.List (countryIso, type, pattern, region);
                 number = numbers.Objects[0].Number;
             }
             return await PowerpackI
-                .Add_NumberAsync(numberpooluuid, number, true);
+                .Add_NumberAsync (numberpooluuid, number, true);
         }
 
-        public override string ToString()
-        {
+        public override string ToString () {
             return "\n" +
-                    "Type: " + Type + "\n" +
-                    "NumberPoolUUID: " + NumberPoolUuid + "\n" +
-                   "Number: " + Number + "\n" +
-                   "CountryISO2: " + CountryIso2 + "\n" +
-                   "AddedOn: " + AddedOn + "\n" +
-                   "AccountPhoneNumberResource: " + AccountPhoneNumberResource + "\n";
+                "Type: " + Type + "\n" +
+                "NumberPoolUUID: " + NumberPoolUuid + "\n" +
+                "Number: " + Number + "\n" +
+                "CountryISO2: " + CountryIso2 + "\n" +
+                "AddedOn: " + AddedOn + "\n" +
+                "AccountPhoneNumberResource: " + AccountPhoneNumberResource + "\n";
         }
     }
 
