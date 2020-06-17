34fa7f578cb203920e8f3a743390d0a516a8d8d3
Kritarth <kritarth@plivo.com>
2020-06-17 14:10:54 +0530

    update jwt example

--- examples/Jwt.cs
+++ examples/Jwt.cs
@@ -8,13 +8,30 @@ namespace Plivo
     {
         public static void Main(string[] args)
         {
-            var token = new Plivo.AccessToken("MADADADADADADADADADA", "qwerty", "username")
+            var token0 = new Plivo.AccessToken("MADADADADADADADADADA", "qwerty", "username")
+                .WithOutgoing(true)
+                .WithIncoming(false)
+                .WithValidFrom(DateTime.Parse("2020-03-22"))
+                .WithLifetime(TimeSpan.FromHours(2))
+                .WithUid("uuid");
+            Console.WriteLine(token0.ToJwt());
+
+            var token1 = new Plivo.AccessToken("MADADADADADADADADADA", "qwerty", "username")
+                .WithOutgoing(true)
+                .WithIncoming(false)
+                .WithValidFrom(DateTime.Parse("2020-03-22"))
+                .WithValidTill(DateTime.Parse("2020-03-23"))
+                .WithUid("uuid");
+            Console.WriteLine(token1.ToJwt());
+
+            var token2 = new Plivo.AccessToken("MADADADADADADADADADA", "qwerty", "username")
                 .WithOutgoing(true)
                 .WithIncoming(false)
                 .WithLifetime(TimeSpan.FromHours(2))
-                .WithValidTill(DateTime.Parse("2020-04-24"))
+                .WithValidTill(DateTime.Parse("2020-03-22"))
                 .WithUid("uuid");
-            Console.WriteLine(token.ToJwt());
+            Console.WriteLine(token2.ToJwt());
+
         }
     }
 }
