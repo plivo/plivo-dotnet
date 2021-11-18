1689d6c4eea8445e65f03bebc40a0a64e16eedf3
Abinaya-plivo <abinaya.s@plivo.com>
2021-11-18 15:20:05 +0530

    Fixed failing UTs in DEVEX-466

--- src/Plivo/Utilities/ComparisonUtilities.cs
+++ src/Plivo/Utilities/ComparisonUtilities.cs
@@ -69,6 +69,10 @@ namespace Plivo.Utilities
                     var actual = target.SelectToken(StringUtilities.SnakeCaseToPascalCase(sourcePair.Key));
                     if (actual == null)
                     {
+                        actual = target.SelectToken(sourcePair.Key);
+                    }
+                    if (actual == null)
+                    {
                         returnString.Append("Key " + StringUtilities.SnakeCaseToPascalCase(sourcePair.Key)
                                                    + " not found" + ", ");
                     }
