3adcba32223391b89ff28e8c553bf73cfbde4f05
narayana <narayana@plivo.com>
2021-12-15 16:29:55 +0530

    test

--- src/Plivo/Resource/Brand/Brand.cs
+++ src/Plivo/Resource/Brand/Brand.cs
@@ -1,6 +1,5 @@
 using Newtonsoft.Json;
-using System;
-using System.Collection.Generic;
+
 namespace Plivo.Resource.Brand
 {
     /// <summary>
--- src/Plivo/Resource/Campaign/Campaign.cs
+++ src/Plivo/Resource/Campaign/Campaign.cs
@@ -1,6 +1,5 @@
 using Newtonsoft.Json;
-using System;
-using System.Collection.Generic;
+
 namespace Plivo.Resource.Campaign
 {
     /// <summary>
