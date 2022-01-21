5ff954233c60abf0a8716466798a08c827bbe0f8
huzaif-plivo <huzaif@plivo.com>
2022-01-21 18:15:07 +0530

    update version.json

--- src/Plivo/Version.cs
+++ src/Plivo/Version.cs
@@ -10,7 +10,7 @@ namespace Plivo
         /// <summary>
         /// DotNet SDK version
         /// </summary>
-        public const string SdkVersion = "5.7.0";
+        public const string SdkVersion = "5.7.1";
         /// <summary>
         /// Plivo API version
         /// </summary>
