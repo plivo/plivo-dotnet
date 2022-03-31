1bd563f94bc2082acc90ab3e35d7c34dc3cbc8ea
narayana <narayana@plivo.com>
2022-04-01 00:14:44 +0530

    profile api

---
+++ src/Plivo/Resource/Profile/Profile.cs
@@ -0,0 +1,118 @@
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Plivo.Resource.Profile
+{
+    /// <summary>
+    /// Profile.
+    /// </summary>
+
+     public class AuthorizedContactResponse {
+        [JsonProperty("email")]
+        public string Email { get; set; }
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("seniority")]
+        public string Seniority { get; set; }
+        [JsonProperty("title")]
+        public string Title { get; set; }
+    }
+
+    public class AuthorizedContact {
+        [JsonProperty("email")]
+        public string Email { get; set; }
+        [JsonProperty("firstName")]
+        public string FirstName { get; set; }
+        [JsonProperty("LastName")]
+        public string LastName { get; set; }
+        [JsonProperty("seniority")]
+        public string Seniority { get; set; }
+        [JsonProperty("title")]
+        public string Title { get; set; }
+    }
+
+    public class Address {
+        [JsonProperty("street")]
+        public string Street { get; set; }
+        [JsonProperty("city")]
+        public string City { get; set; }
+        [JsonProperty("state")]
+        public string State { get; set; }
+        [JsonProperty("postal_code")]
+        public string PostalCode { get; set; }
+        [JsonProperty("country")]
+        public string Country { get; set; }
+    }
+
+    
+    [JsonObject(MemberSerialization.OptIn)]
+    public class ProfileResponse: Resource{
+
+        [JsonProperty("profile_uuid")]
+        public Profile ProfileUUID {get; set;}
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this, Formatting.Indented);
+        }
+    }
+
+    public class Profile 
+    {
+        [JsonProperty("address")]
+        public string Address { get; set; }
+
+        [JsonProperty("alt_business_id_type")]
+        public string AltBusinessIdType { get; set; }
+
+        [JsonProperty("authorized_contact")]
+        public AuthorizedContact authorized_contact { get; set; }
+
+        [JsonProperty("company_name")]
+        public string CompanyName { get; set; }
+
+        [JsonProperty("customer_type")]
+        public string CustomerType { get; set; }
+
+        [JsonProperty("ein_issuing_country")]
+        public string EinIssuingCountry { get; set; }  
+
+        [JsonProperty("entity_type")]
+        public string EntityType { get; set; }  
+
+        [JsonProperty("primary_profile")]
+        public string PrimaryProfile { get; set; }  
+
+        [JsonProperty("profile_type")]
+        public string ProfileType { get; set; }  
+
+        [JsonProperty("profile_alias")]
+        public string ProfileAlias { get; set; } 
+
+        [JsonProperty("profile_uuid")]
+        public string ProfileUuid { get; set; }  
+
+        [JsonProperty("stock_symbol")]
+        public string StockSymbol { get; set; }  
+
+        [JsonProperty("vertical")]
+        public string Vertical { get; set; }  
+
+         [JsonProperty("website")]
+        public string Website { get; set; }
+
+    }
+
+    [JsonObject(MemberSerialization.OptIn)]
+    public class GetProfile: Resource
+    {
+        [JsonProperty("profile")]
+        public Profile Profile {get; set;}
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this, Formatting.Indented);
+        }
+    }
+}
---
+++ src/Plivo/Resource/Profile/ProfileInterface.cs
@@ -0,0 +1,268 @@
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Threading.Tasks;
+using Plivo.Client;
+
+namespace Plivo.Resource.Profile
+{
+    /// <summary>
+    /// Profile interface.
+    /// </summary>
+    public class ProfileInterface : ResourceInterface
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:plivo.Resource.Profile.ProfileInterface"/> class.
+        /// </summary>
+        /// <param name="client">Client.</param>
+        public ProfileInterface(HttpClient client) : base(client)
+        {
+            Uri = "Account/" + Client.GetAuthId() + "/";
+        }
+
+        #region GET
+         /// <summary>
+        /// Get Profile with the specified uuid.
+        /// </summary>
+        /// <returns>The get.</returns>
+        /// <param name="profileUuid">profileUuid.</param>
+        public GetProfile Get(string profileUuid)
+        {
+			return ExecuteWithExceptionUnwrap(() =>
+			{
+				var response = Task.Run(async () => await GetResource<GetProfile>("Profile/"+profileUuid).ConfigureAwait(false)).Result;
+				return response;
+			});
+        }
+        /// <summary>
+        /// Asynchronously get Profile with the specified profileUuid.
+        /// </summary>
+        /// <returns>The get.</returns>
+        /// <param name="profileUuid"> profileUuid.</param>
+        public async Task<GetProfile> GetAsync(string profileUuid)
+        {
+            var response = await GetResource<GetProfile>("Profile/"+profileUuid);
+            return response;
+        }
+        #endregion
+
+        #region List
+        /// <summary>
+        /// <summary>
+        /// List profile
+        /// </summary>
+        /// <returns>The list.</returns>
+        /// <param name="limit">limit.</param>
+        /// <param name="offset">offset.</param>
+        public ProfileListResponse<ListProfiles> List()
+        {
+			return ExecuteWithExceptionUnwrap(() =>
+			{
+				var resources = Task.Run(async () => await ListResources<ProfileListResponse<ListProfiles>>("Profile",null).ConfigureAwait(false)).Result;
+                resources.Campaigns.ForEach(
+					(obj) => obj.Interface = this
+				);
+				return resources;
+			});
+        }
+        /// <summary>
+        /// <summary>
+        /// List Brand list type and status.
+        /// </summary>
+        /// <returns>The list.</returns>
+        /// <param name="brand">brand.</param>
+        /// <param name="usecase">status.</param>
+        public async Task<ProfileListResponse<ListProfiles>> ListAsync()
+        {
+           
+            var resources = await ListResources<ProfileListResponse<ListProfiles>>("Profile", null);
+            resources.Campaigns.ForEach(
+					(obj) => obj.Interface = this
+				);
+            return resources;
+        }
+        #endregion
+
+        #region Create
+        /// <summary>
+        /// Create Profile with the specified fields
+        /// </summary>
+        /// <returns>The create.</returns>
+        /// <param name="profile_alias">profile_alias.</param>
+        /// <param name="profile_type">profile_type.</param>
+        /// <param name="customer_type">customer_type.</param>
+        /// <param name="entity_type">entity_type.</param>
+        /// <param name="company_name">company_name.</param>
+        /// <param name="ein">ein</param>
+        /// <param name="vertical">vertical</param>
+        /// <param name="ein_issuing_country">ein_issuing_country</param>
+        /// <param name="stock_symbol">stock_symbol</param>
+        /// <param name="stock_exchange">stock_exchange</param>
+        /// <param name="alt_business_id_type">alt_business_id_type</param>
+        /// <param name="authorized_contact">authorized_contact</param>
+        /// <param name="address">address</param>
+
+        public ProfileResponse Create(string profile_alias, string profile_type,  string customer_type, 
+            string entity_type, string company_name,  string ein, string vertical,
+            string ein_issuing_country, string stock_symbol, string stock_exchange, string alt_business_id_type, AuthorizedContact authorized_contact, Address address)
+        {
+        var data = CreateData(
+            new
+            {
+                profile_alias,
+                profile_type,
+                customer_type,
+                entity_type,
+                company_name,
+                ein,
+                vertical,
+                ein_issuing_country,
+                stock_symbol,
+                stock_exchange,
+                alt_business_id_type,
+                authorized_contact,
+                address
+            });
+		return ExecuteWithExceptionUnwrap(() =>
+		{
+				var result = Task.Run(async () => await Client.Update<ProfileResponse>(Uri + "Profile/", data).ConfigureAwait(false)).Result;
+				result.Object.StatusCode = result.StatusCode;
+                return result.Object;
+		});
+        }
+
+		 /// <summary>
+        /// Create Profile with the specified fields
+        /// </summary>
+        /// <returns>The create.</returns>
+        /// <param name="profile_alias">profile_alias.</param>
+        /// <param name="profile_type">profile_type.</param>
+        /// <param name="customer_type">customer_type.</param>
+        /// <param name="entity_type">entity_type.</param>
+        /// <param name="company_name">company_name.</param>
+        /// <param name="ein">ein</param>
+        /// <param name="vertical">vertical</param>
+        /// <param name="ein_issuing_country">ein_issuing_country</param>
+        /// <param name="stock_symbol">stock_symbol</param>
+        /// <param name="stock_exchange">stock_exchange</param>
+        /// <param name="alt_business_id_type">alt_business_id_type</param>
+        /// <param name="authorized_contact">authorized_contact</param>
+        /// <param name="address">address</param>
+		public async Task<ProfileResponse> CreateAsync(string profile_alias, string profile_type,  string customer_type, 
+            string entity_type, string company_name,  string ein, string vertical,
+            string ein_issuing_country, string stock_symbol, string stock_exchange, string alt_business_id_type, AuthorizedContact authorized_contact, Address address)
+		{
+          var data = CreateData(
+            new
+            {
+                profile_alias,
+                profile_type,
+                customer_type,
+                entity_type,
+                company_name,
+                ein,
+                vertical,
+                ein_issuing_country,
+                stock_symbol,
+                stock_exchange,
+                alt_business_id_type,
+                authorized_contact,
+                address
+            });
+			var result = await Client.Update<ProfileResponse>(Uri + "Profile/", data);
+            result.Object.StatusCode = result.StatusCode;
+			return result.Object;
+		}
+		#endregion
+
+        #region Update
+        /// <summary>
+        /// Update Profile with the specified fields
+        /// </summary>
+        /// <returns>The create.</returns>
+        /// <param name="profile_uuid">profileUuid</param>
+        /// <param name="company_name">company_name.</param>
+        /// <param name="entity_type">entity_type.</param>
+        /// <param name="website">website.</param>
+        /// <param name="entity_type">entity_type.</param>
+        /// <param name="vertical">vertical.</param>
+        /// <param name="authorized_contact">authorized_contact</param>
+        /// <param name="address">address</param>
+
+        public GetProfile Create(string profile_uuid, string company_name =null,  string website = null, 
+            string entity_type=null, string vertical= null,  AuthorizedContact authorized_contact=null, Address address=null)
+        {
+        var data = CreateData(
+            new
+            {
+                company_name,
+                website,
+                entity_type,
+                authorized_contact,
+                address,
+                vertical
+            });
+		return ExecuteWithExceptionUnwrap(() =>
+		{
+				var result = Task.Run(async () => await Client.Update<GetProfile>(Uri + "Profile/"+profile_uuid+"/", data).ConfigureAwait(false)).Result;
+				result.Object.StatusCode = result.StatusCode;
+                return result.Object;
+		});
+        }
+
+		         /// <summary>
+        /// Update Profile with the specified fields
+        /// </summary>
+        /// <returns>The create.</returns>
+        /// <param name="profile_uuid">profileUuid</param>
+        /// <param name="company_name">company_name.</param>
+        /// <param name="entity_type">entity_type.</param>
+        /// <param name="website">website.</param>
+        /// <param name="entity_type">entity_type.</param>
+        /// <param name="vertical">vertical.</param>
+        /// <param name="authorized_contact">authorized_contact</param>
+        /// <param name="address">address</param>
+		public async Task<ProfileResponse> CreateAsync(string profile_uuid, string company_name =null,  string website = null, 
+            string entity_type=null, string vertical= null,  AuthorizedContact authorized_contact=null, Address address=null)
+        {
+        var data = CreateData(
+            new
+            {
+                company_name,
+                website,
+                entity_type,
+                authorized_contact,
+                address,
+                vertical
+            });
+			var result = await Client.Update<ProfileResponse>(Uri + "Profile/"+profile_uuid+"/", data);
+            result.Object.StatusCode = result.StatusCode;
+			return result.Object;
+		}
+		#endregion
+
+        #region Delete
+        /// <summary>
+        ///  delete Profile with the specified profileuuid.
+        /// </summary>
+        /// <returns>The delete.</returns>
+        /// <param name="uuid">profile identifier.</param>
+         public DeleteResponse<Profile> Delete(string profileUuid)
+        {
+			return ExecuteWithExceptionUnwrap(() =>
+			{
+				return Task.Run(async () => await DeleteResource<DeleteResponse<Profile>>(profileUuid).ConfigureAwait(false)).Result;
+			});
+        }
+        /// <summary>
+        /// Asynchronously delete Profile with the specified profileuuid.
+        /// </summary>
+        /// <returns>The delete.</returns>
+        /// <param name="profileID">profileUUId.</param>
+        public async Task<DeleteResponse<Profile>> DeleteAsync(string profileUuid)
+        {
+            return await DeleteResource<DeleteResponse<Profile>>(profileUuid);
+        }
+        #endregion
+    }
+}
