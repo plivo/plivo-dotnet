484542ab4b7cf3b9eb5315f337d55a635a63623b
NirmitiJain <nirmiti.jain@plivo.com>
2022-10-10 09:16:00 +0530

    ut

--- tests_netcore/Plivo.NetCore.Test/Resources/TestCampaign.cs
+++ tests_netcore/Plivo.NetCore.Test/Resources/TestCampaign.cs
@@ -110,6 +110,7 @@ namespace Plivo.NetCore.Test.Resources
                 response
             );
             
+            var resp = Api.Campaign.Get(id);
             // Assert.Empty(
             //     ComparisonUtilities.Compare(
             //         response,
