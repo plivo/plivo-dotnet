c1fa61da5f7f5f8cbb380e23b5e5bab6c511dc81
kalyan-plivo <kalyan.sunku@plivo.com>
2023-05-04 13:29:09 +0530

    bump version

--- src/Plivo/Version.cs
+++ src/Plivo/Version.cs
@@ -10,7 +10,7 @@ namespace Plivo
         /// <summary>
         /// DotNet SDK version
         /// </summary>
-        public const string SdkVersion = "5.26.0";
+        public const string SdkVersion = "5.27.0";
         /// <summary>
         /// Plivo API version
         /// </summary>
