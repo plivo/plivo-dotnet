d6bc99eeae0a7c3ecc2d465897a4e3ef322bc855
renoldthomas-plivo <renold.thomas@plivo.com>
2023-10-16 19:32:11 +0530

    version update

--- src/Plivo/Version.cs
+++ src/Plivo/Version.cs
@@ -10,7 +10,11 @@ namespace Plivo
         /// <summary>
         /// DotNet SDK version
         /// </summary>
+<<<<<<< Updated upstream
         public const string SdkVersion = "5.34.0";
+=======
+        public const string SdkVersion = "5.35.0";
+>>>>>>> Stashed changes
         /// <summary>
         /// Plivo API version
         /// </summary>
